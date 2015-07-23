7a2f28c1c8657ca69d22f79a76c8c09aa7c4d452
Milo Spencer-Harper <miloharper@gmail.com>
2015-07-23 15:24:34 +0800

    Made console output easier to read.

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class NeuralNetwork():
             layer2_delta = layer2_error * self.__sigmoid_derivative(output_from_layer_2)
 
             # Calculate the error for layer 1 (By looking at the weights in layer 1,
-            # we can determine by how much layer 1 contributed to the error in layer 2?)
+            # we can determine by how much layer 1 contributed to the error in layer 2).
             layer1_error = layer2_delta.dot(self.layer2.synaptic_weights.T)
             layer1_delta = layer1_error * self.__sigmoid_derivative(output_from_layer_1)
 
@@ -54,6 +54,13 @@ class NeuralNetwork():
         output_from_layer2 = self.__sigmoid(dot(output_from_layer1, self.layer2.synaptic_weights))
         return output_from_layer1, output_from_layer2
 
+    # The neural network prints its weights
+    def print_weights(self):
+        print "    Layer 1 (4 neurons, each with 3 inputs): "
+        print self.layer1.synaptic_weights
+        print "    Layer 2 (1 neuron, with 4 inputs):"
+        print self.layer2.synaptic_weights
+
 if __name__ == "__main__":
 
     #Seed the random number generator
@@ -68,24 +75,22 @@ if __name__ == "__main__":
     # Combine the layers to create a neural network
     neural_network = NeuralNetwork(layer1, layer2)
 
-    print "Random starting synaptic weights: "
-    print neural_network.layer1.synaptic_weights
-    print neural_network.layer2.synaptic_weights
+    print "Stage 1) Random starting synaptic weights: "
+    neural_network.print_weights()
 
     # The training set. We have 7 examples, each consisting of 3 input values
     # and 1 output value.
     training_set_inputs = array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1], [0, 0, 0]])
     training_set_outputs = array([[0, 1, 1, 1, 1, 0, 0]]).T
 
-    # Train the neural network using a training set.
+    # Train the neural network using the training set.
     # Do it 60,000 times and make small adjustments each time.
     neural_network.train(training_set_inputs, training_set_outputs, 60000)
 
-    print "New synaptic weights after training: "
-    print neural_network.layer1.synaptic_weights
-    print neural_network.layer2.synaptic_weights
+    print "Stage 2) New synaptic weights after training: "
+    neural_network.print_weights()
 
     # Test the neural network with a new situation.
-    print "Considering a new situation [1, 1, 0] -> ?: "
+    print "Stage 3) Considering a new situation [1, 1, 0] -> ?: "
     hidden_state, output = neural_network.think(array([1, 1, 0]))
     print output
