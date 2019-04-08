260a198029030c46bb5c8e8d953441a0f308134c
Milo Spencer-Harper <miloharper@gmail.com>
2019-04-08 14:09:10 +0100

    Updated code from Python 2 to Python 3.

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class NeuralNetwork():
     # We train the neural network through a process of trial and error.
     # Adjusting the synaptic weights each time.
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        for iteration in xrange(number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
             # Pass the training set through our neural network
             output_from_layer_1, output_from_layer_2 = self.think(training_set_inputs)
 
@@ -56,10 +56,10 @@ class NeuralNetwork():
 
     # The neural network prints its weights
     def print_weights(self):
-        print "    Layer 1 (4 neurons, each with 3 inputs): "
-        print self.layer1.synaptic_weights
-        print "    Layer 2 (1 neuron, with 4 inputs):"
-        print self.layer2.synaptic_weights
+        print("    Layer 1 (4 neurons, each with 3 inputs):")
+        print(self.layer1.synaptic_weights)
+        print("    Layer 2 (1 neuron, with 4 inputs):")
+        print(self.layer2.synaptic_weights)
 
 if __name__ == "__main__":
 
@@ -75,7 +75,7 @@ if __name__ == "__main__":
     # Combine the layers to create a neural network
     neural_network = NeuralNetwork(layer1, layer2)
 
-    print "Stage 1) Random starting synaptic weights: "
+    print("Stage 1) Random starting synaptic weights: ")
     neural_network.print_weights()
 
     # The training set. We have 7 examples, each consisting of 3 input values
@@ -87,10 +87,10 @@ if __name__ == "__main__":
     # Do it 60,000 times and make small adjustments each time.
     neural_network.train(training_set_inputs, training_set_outputs, 60000)
 
-    print "Stage 2) New synaptic weights after training: "
+    print("Stage 2) New synaptic weights after training: ")
     neural_network.print_weights()
 
     # Test the neural network with a new situation.
-    print "Stage 3) Considering a new situation [1, 1, 0] -> ?: "
+    print("Stage 3) Considering a new situation [1, 1, 0] -> ?: ")
     hidden_state, output = neural_network.think(array([1, 1, 0]))
-    print output
+    print(output)
